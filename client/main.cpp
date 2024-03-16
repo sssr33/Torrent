fdbe05876a89db84708bcee17426f616559fa5c7
Alexander Chaikovsky <sskr33@gmail.com>
2024-03-16 16:12:35 +0200

    [TR-02] minimal code to test use of libtorrent, enabled /Zc:__cplusplus

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,16 @@
 ﻿
+// order in which boost sees the correct value of _WIN32_WINNT
+// https://stackoverflow.com/questions/9750344/boostasio-winsock-and-winsock-2-compatibility-issue
+// https://github.com/boostorg/beast/issues/1895
+#include <SDKDDKVer.h>
+#include <boost/asio.hpp>
+#include <Windows.h>
+
+#include <cstdlib>
+#include <libtorrent/libtorrent.hpp>
 
 int main() {
+	lt::session s;
+
 	return 0;
 }
